e50ddb58ffd8ac5599d9d1682a4f0e8b34883273
Nathan Chancellor <natechancellor@gmail.com>
2016-10-12 17:41:46 -0700

    AnyKernel: Add fstab.bullhead and change kernel.name
    Add fstab to disable forced encryption and dm-verity and reflect this change in the kernel.name variable
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=Stock Bullhead Kernel
+kernel.string=Upstreamed Bullhead Kernel with disabled forced encryption and dm-verity
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
