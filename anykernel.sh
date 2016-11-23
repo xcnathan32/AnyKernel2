df5b109296d58ba23fe990940813ebdf5d9fa825
franciscofranco <franciscofranco.1990@gmail.com>
2016-11-23 02:33:22 -0700

    AnyKernel: Add support for FKU's performance profiles and use Franco's profile
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -46,6 +46,9 @@ dump_boot;
 # init.flash.rc
 insert_line init.bullhead.rc "init.flash.rc" after "import init.bullhead.misc.rc" "import init.flash.rc"
 
+# init.performance_profiles.rc
+insert_line init.bullhead.rc "performance_profiles" after "import init.bullhead.ramdump.rc" "import init.performance_profiles.rc";
+
 # Add frandom compatibility
 backup_file ueventd.rc;
 insert_line ueventd.rc "frandom" after "urandom" "/dev/frandom              0666   root       root\n";
