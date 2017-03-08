208ef461b40fddbeadbf70765a2eef1dbe3affdb
Nathan Chancellor <natechancellor@gmail.com>
2017-03-08 02:14:14 -0700

    Ramdisk: Add back our files
    History for these can be viewed in other branches
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -31,10 +31,43 @@ is_slot_device=0;
 chmod -R 755 $ramdisk
 
 
+## Alert of unsupported Android version
+android_ver=$(mount /system; grep "^ro.build.version.release" /system/build.prop | cut -d= -f2; umount /system);
+case "$android_ver" in
+  "7.0"|"7.1"|"7.1.1") compatibility_string="your version is unsupported, expect no support!";;
+  "7.1.2") compatibility_string="your version is supported!";;
+esac;
+ui_print "Running Android $android_ver, $compatibility_string";
+
+
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
+
+
+# init.flash.rc
+insert_line init.angler.rc "init.flash.rc" after "import init.angler.sensorhub.rc" "import init.flash.rc";
+
+# Add frandom compatibility
+backup_file ueventd.angler.rc;
+insert_line ueventd.angler.rc "erandom" before "/dev/genlock" "/dev/erandom              0666   root       root\n";
+insert_line ueventd.angler.rc "frandom" before "/dev/genlock" "/dev/frandom              0666   root       root\n";
+backup_file file_contexts;
+insert_line file_contexts "frandom" after "urandom" "/dev/frandom		u:object_r:frandom_device:s0\n";
+insert_line file_contexts "erandom" after "urandom" "/dev/erandom		u:object_r:erandom_device:s0\n";
+
+# irq balance
+replace_string init.angler.rc "service msm_irqbalance /vendor/bin/msm_irqbalance -f /msm_irqbalance.conf" "service msm_irqbalance /vendor/bin/msm_irqbalance -f /vendor/etc/msm_irqbalance.conf" "service msm_irqbalance /vendor/bin/msm_irqbalance -f /msm_irqbalance.conf"
+
+# Set permissive on boot - but only if not already permissive
+cmdfile=`ls $split_img/*-cmdline`;
+cmdtmp=`cat $cmdfile`;
+case "$cmdtmp" in
+  *selinux=permissive*) ;;
+  *) rm $cmdfile; echo "androidboot.selinux=permissive $cmdtmp" > $cmdfile;;
+esac;
+
 
 # end ramdisk changes
 
